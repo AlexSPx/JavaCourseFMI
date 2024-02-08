dd58e83d4286f17cd868f08fefd4c1481b464bef
alexspx <alexspx@hotmail.com>
2024-02-08 16:36:38 +0200

    removed project folder

--- project/AuthenticationServer/src/bg/sofia/uni/fmi/mjt/authserver/response/Response.java
+++
@@ -1,9 +0,0 @@
-package bg.sofia.uni.fmi.mjt.authserver.response;
-
-public record Response(String message, int status) {
-    @Override
-    public String toString() {
-        return String.format("{ \"message\": \"%s\", \"status\": \"%d\"}"
-                , message, status);
-    }
-}
--- project/AuthenticationServer/src/bg/sofia/uni/fmi/mjt/authserver/user/Role.java
+++
@@ -1,6 +0,0 @@
-package bg.sofia.uni.fmi.mjt.authserver.user;
-
-public enum Role {
-    NORMAL,
-    ADMIN
-}
